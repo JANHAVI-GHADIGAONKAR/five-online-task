670b9191849037cc4612215301009aec827947d0
ADMIN <janhavi.ghadi09@gmail.com>
2024-06-12 17:40:16 +0530

    first section done

---
+++ script.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const images = document.querySelectorAll('header .slider img');
+//     let currentImage = 0;
+
+//     function changeImage() {
+//         images[currentImage].style.opacity = 0;
+//         currentImage = (currentImage + 1) % images.length;
+//         images[currentImage].style.opacity = 1;
+//     }
+
+//     setInterval(changeImage, 3000);
+
+//     const textSlides = document.querySelectorAll('header .text-slider .text-slide');
+//     let currentTextSlide = 0;
+
+//     function changeText() {
+//         textSlides[currentTextSlide].classList.remove('active');
+//         currentTextSlide = (currentTextSlide + 1) % textSlides.length;
+//         textSlides[currentTextSlide].classList.add('active');
+//     }
+
+//     setInterval(changeText, 3000);
+// });
